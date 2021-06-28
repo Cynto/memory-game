d565c91243aa853165778f8f09fe4c13e088fc11
Cynto <lucagaravello@outlook.com>
2021-06-28 16:59:26 +0200

    Changed mobile layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Scoreboard from './Scoreboard'
 function Header(props) {
   return (
     <header>
-      <h1>Symbol Memory Game</h1>
+      <h1 className="title">Symbol Memory Game</h1>
       <Scoreboard bestScore={props.bestScore} score={props.score}/>
     </header>
   )
